aeb18924977c0905f37f793fe9ff06d0bf0cafe3
OrayKT <oray.kt@gmail.com>
2019-03-07 17:19:34 +0100

    api/movies/:movie_id

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ errorMessage: err.message });
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -4,14 +4,27 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 
 router.get('/', (req, res) => {
-
   const promise = Movie.find({});
-  promise.then((data) => {
-    res.json(data);
+  promise.then((movie) => {
+    res.json(movie);
   }).catch((err) => {
     console.error(err);
-    res.send("Unexpected Error!");
+    res.send(err.message);
   });
+});
+
+router.get("/:movie_id", (req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+    res.json(movie);
+  }).catch((err) => {
+    if (err.name === "CastError") {
+      next({ message: "The movie was not found!" });
+    } else {
+      console.error(err);
+      res.send("Unexpected Error!");
+    }
+  })
 });
 
 router.post('/', (req, res, next) => {
@@ -21,7 +34,7 @@ router.post('/', (req, res, next) => {
     res.json({ status: res.statusCode });
   }).catch((err) => {
     console.error(err);
-    res.send("Unexpected Error!");
+    res.send(err.message);
   });
 });
 
