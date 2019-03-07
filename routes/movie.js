08a31d86490ad20bbb4835cf85941b0cdb86bf30
OrayKT <oray.kt@gmail.com>
2019-03-07 17:38:57 +0100

    put /api/movies/:movie_id

--- routes/movie.js
+++ routes/movie.js
@@ -6,7 +6,7 @@ const Movie = require('../models/Movie');
 router.get('/', (req, res) => {
   const promise = Movie.find({});
   promise.then((movie) => {
-    res.json(movie);
+    res.json({ statusCode: res.statusCode, movie });
   }).catch((err) => {
     console.error(err);
     res.send(err.message);
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 router.get("/:movie_id", (req, res, next) => {
   const promise = Movie.findById(req.params.movie_id);
   promise.then((movie) => {
-    res.json(movie);
+    res.json({ statusCode: res.statusCode, movie });
   }).catch((err) => {
     if (err.name === "CastError") {
       next({ message: "The movie was not found!" });
@@ -30,12 +30,27 @@ router.get("/:movie_id", (req, res, next) => {
 router.post('/', (req, res, next) => {
   //const { title, category, country, year, imdb_score, director_id } = req.body;
   const promise = new Movie(req.body).save();
-  promise.then((data) => {
-    res.json({ status: res.statusCode });
+  promise.then((movie) => {
+    res.json({ statusCode: res.statusCode, added: movie });
   }).catch((err) => {
     console.error(err);
     res.send(err.message);
   });
 });
 
+router.put("/:movie_id", (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id, req.body, { new: true }
+  );
+  promise.then((movie) => {
+    res.json({ statusCode: res.statusCode, updated: movie });
+  }).catch((err) => {
+    if (err.name === "CastError") {
+      next({ message: "The movie was not found!" });
+    } else {
+      console.error(err);
+      res.send("Unexpected Error!");
+    }
+  })
+});
 module.exports = router;
