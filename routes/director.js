ce140e391b32f24a3f694083553f8e5b9a1c092d
OrayKT <oray.kt@gmail.com>
2019-03-08 16:11:17 +0100

    get /api/directors/:director_id

--- routes/director.js
+++ routes/director.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose');
 const Director = require('../models/Director');
 
 router.get('/', (req, res, next) => {
@@ -30,6 +30,57 @@ router.get('/', (req, res, next) => {
                 }
             }
         }, {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ]);
+    promise.then((directors) => {
+        res.json(directors);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get('/:director_id', (req, res, next) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
             $project: {
                 _id: '$_id._id',
                 name: '$_id.name',
