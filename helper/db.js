d3858ae5f77f3a4657e9e8d72720bdd75301f285
OrayKT <oray.kt@gmail.com>
2019-03-09 17:01:32 +0100

    Mongodb live

--- helper/db.js
+++ helper/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/movie-api', { useNewUrlParser: true });
+    mongoose.connect('mongodb+srv://testuser:testpassword@cluster0-t6vth.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
     mongoose.set('useCreateIndex', true);
 
     mongoose.connection.on('open', () => {
-        //console.log('MongoDB: Connected!');
+        console.log('MongoDB: Connected!');
     });
     mongoose.connection.on('error', (error) => {
-        //console.log('MongoDB: Connection Failed! \n', error);
+        console.log('MongoDB: Connection Failed! \n', error);
     });
 
     mongoose.Promise = global.Promise;
